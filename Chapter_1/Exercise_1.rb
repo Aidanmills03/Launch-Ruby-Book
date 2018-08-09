4b45dda6cb011cf7a4c32b743879c89864e194d2
Aidan Mills <aidanmills03@gmail.com>
2018-08-09 04:57:51 -0400

    Finish Exercise 1, move onto second exercise.

---
+++ Chapter_1/Exercise_1.rb
@@ -0,0 +1,28 @@
+  #  puts "Aidan " + "Mills"
+  # puts 15.0 / 3.0
+
+  movies = { Step_Brothers: 2008,
+             Austin_Powers: 1999,
+             Waynes_World: 1992 }
+
+  puts movies [:Step_Brothers]
+  puts movies [:Austin_Powers]
+  puts movies [:Waynes_World]
+
+
+  dates = [2008, 1999, 1992]
+
+  puts dates [0]
+  puts dates [1]
+  puts dates [2]
+
+
+  puts 5 * 4 * 3 * 2 * 1
+  puts 6 * 5 * 4 * 3 * 2 * 1
+  puts 7 * 6 * 5 * 4 * 3 * 2 * 1
+  puts 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1
+
+
+  puts 327.11 * 327.11
+  puts 648.22 * 648.22
+  puts 996.44 * 996.44
