4f65cda7821363469510d52b16d7102bd9af774e
Aidan Mills <aidanmills03@gmail.com>
2018-08-09 05:36:24 -0400

    Started Variable exercise

---
+++ Chapter_1/name.rb
@@ -0,0 +1,15 @@
+  #puts "What is your name?"
+  #name = gets.chomp
+  #puts "Hello, " + name + ", how are you?"
+
+  #puts "What is your name? "
+  #ame = gets.chomp
+  #10.times do
+  #  puts name
+  #end
+
+  puts "What is your first name?"
+  first_name = gets.chomp
+  puts "What is your last name?"
+  last_name = gets.chomp
+  puts "Hello, " + first_name + " " + last_name
