d74aae4855c1930ea34219681dab2c8126fd3a33
Aidan Mills <aidanmills03@gmail.com>
2018-08-16 08:56:52 -0400

    finish arrays

---
+++ Chapter_1/exercise_5.rb
@@ -0,0 +1,32 @@
+  arr = [1, 3, 5, 7, 9, 11]
+  if arr.include?(3)
+    puts "Yes!"
+  else
+    puts "No!"
+  end
+
+  #arr = ["b", "a"]
+  #rr = arr.product([Array(1..3)])
+#  arr.first.delete(arr.first.last)
+#  puts arr
+
+    arr = [["test", "hello", "world"],["example", "mem"]]
+    puts arr.last.first
+
+#  array = [15, 7, 18, 5, 12, 8, 5, 1]
+#  array[5]
+
+
+  string = "Welcome to America"
+  a = string[6]
+  b = string [11]
+  c = string [19]
+
+  names = ["bob", "joe", "susan", "margaret"]
+  names[3] = "jody"
+  p names
+
+
+  my_arr = [1, 2, 3, 4, 5]
+  p my_arr
+  my_arr.each { |x| p x * 2}
